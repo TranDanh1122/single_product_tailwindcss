57d52bf7033fd14af20532e55f3228ff07d3bbfd
TranDanh1122 <trandanh14042000@gmail.com>
2024-12-22 11:24:10 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'orange': ' hsl(26, 100%, 55%)',
+        'pale_orange': 'hsl(25, 100%, 94%)',
+        'very_dark_blue': 'hsl(220, 13%, 13%)',
+        'dark_grayish_blue': 'hsl(219, 9%, 45%)',
+        'grayish_blue': 'hsl(220, 14%, 75%)',
+        'light_grayish_blue': 'hsl(223, 64%, 98%)',
+        'white': 'hsl(0, 0%, 100%)',
+        'black': 'hsla(0, 0%, 0% , 0.5)',
+      }
+    },
+  },
+  plugins: [],
+}
+
